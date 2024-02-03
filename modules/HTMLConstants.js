7e87e475440cc6713b4b18ddda5e4e4a8b01eb25
Omega <castoretpapillon@gmail.com>
2024-02-03 02:23:48 +0100

    Meta color fix

--- modules/HTMLConstants.js
+++ modules/HTMLConstants.js
@@ -26,7 +26,7 @@ let module = {
         <link rel="icon" href="/favicon">
         <link rel="stylesheet" href="/files/main.css">
         <meta property="og:title" content="${title}" />
-        <meta name="theme-color" content="#36393F">
+        <meta name="theme-color" content="#36393F"/>
         `;  
 
     },
